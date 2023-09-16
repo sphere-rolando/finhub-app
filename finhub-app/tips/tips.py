05112ceb5e3ef74ac4109cca89b42456ad1562df
Rolando Diaz Cruz <rolando@sphere-enterprises.com>
2023-09-16 15:43:43 -0400

    Updated openai prompt

--- finhub-app/tips/tips.py
+++ finhub-app/tips/tips.py
@@ -3,7 +3,8 @@ from ..models.gpt import ask_gpt
 
 tips_blueprint = Blueprint("tips", __name__)
 
-@tips_blueprint.route("/<subject>", methods=["GET"])
-def generate_tips(subject):
-    gpt_answer = ask_gpt(f"Act as if you were an advisor. Give me a tip in regard to {subject}")
+@tips_blueprint.route("/", methods=["GET"])
+def generate_tips():
+    # Kept getting an answer related to creating a budget so had to modify the prompt to make it more inclusive of other strategies
+    gpt_answer = ask_gpt(f"In 1-2 sentences, give me a random tip that could help me make better decisions when it comes to handling my personal finances. Do not include any tips that mention a budget.")
     return gpt_answer
