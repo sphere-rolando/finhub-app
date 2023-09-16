a2fd0d1424677c3379601eab4bfd17ca00648c16
Rolando Diaz Cruz <rolando@sphere-enterprises.com>
2023-09-16 11:09:31 -0400

    Update - added web blueprint for rendering html

--- finhub-app/__init__.py
+++ finhub-app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from .tips.tips import tips_blueprint
 from .news.news import news_blueprint
+from .web.web import web_blueprint
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -10,10 +11,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
-    def index():
-        return 'Hello, World!'
-    
+    app.register_blueprint(web_blueprint, url_prefix="/")
     app.register_blueprint(tips_blueprint, url_prefix="/api/tips")
     app.register_blueprint(news_blueprint, url_prefix="/api/news")
 
